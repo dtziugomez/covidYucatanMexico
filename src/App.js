c73f7a2c1dfcb0b56bd1c7608cf8664c6efbf954
dtziugomez <dtziugomez@gmail.com>
2020-06-26 22:42:58 -0500

    using api from municipios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      municipios: []
+    };
+  }
+
+  fetchMunicipios = () => {
+    fetch(
+      "https://api.apify.com/v2/key-value-stores/8uKXvLNo1rQA7Tt9B/records/LATEST?disableRedirect=1"
+    )
+      .then(response => response.json())
+      .then(municipios => {
+        this.setState({ municipios: municipios.State });
+      });
+  };
+
+  componentDidMount() {
+    this.fetchMunicipios();
+  }
+  render() {
+    return (
+      <>
+        {this.state.municipios.length > 0 && (
+          <ul>
+            {this.state.municipios.map(municipio => (
+              <li key={municipio.clave}>
+                Municipio:{municipio.nombre}-Confirmados:{municipio.confirmados}
+              </li>
+            ))}
+          </ul>
+        )}
+      </>
+    );
+  }
 }
-
-export default App;
